e844eeefd9fdbc8553597f3b0fd043e40e0bc15d
Zach Fedor <zachfedor@gmail.com>
2025-11-04 09:51:17 -0500

    fix: clarify reference not found error

--- src/lib.rs
+++ src/lib.rs
@@ -331,6 +331,8 @@ fn get_hash(repo: &Repo) -> Result<String, Box<dyn Error>> {
                 return Ok(r.hash.clone());
             }
         }
+
+        return Err(format!("Reference not found: {gitref}").into());
     }
 
     Err("Reference not found.".into())
